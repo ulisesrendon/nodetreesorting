03dd1a92fe4eb42c84fd7a4dd7489af6a9b80407
Ulises Rendon <fdulises@outlook.com>
2024-03-26 12:37:50 -0600

    node three and node sorting working

--- js/order.js
+++ js/order.js
@@ -2,15 +2,21 @@ const mainItem = document.createElement('div');
 mainItem.classList.add('list-group-item');
 mainItem.classList.add('nested');
 
-function nestedItem(title) {
+function fieldRender(title) {
     const newItem = mainItem.cloneNode();
     newItem.innerHTML = `${title} <div class="list-group nested-sortable"></div>`;
+    new Sortable(newItem, {
+        group: 'nested',
+        animation: 150,
+        fallbackOnBody: true,
+        swapThreshold: 0.65
+    });
     return newItem;
 }
 
 document.addEventListener('DOMContentLoaded', function(){
 
-    const entityMain = document.querySelector("#entity-main");
+    const entityMain = document.querySelector("#entityMain");
     new Sortable(entityMain, {
         group: 'nested',
         animation: 150,
@@ -18,15 +24,17 @@ document.addEventListener('DOMContentLoaded', function(){
         swapThreshold: 0.65
     });
 
-    for(let i = 0; i<data.length; i++){
-        const entityItem = nestedItem(data[i].title);
-        new Sortable(entityItem, {
-            group: 'nested',
-            animation: 150,
-            fallbackOnBody: true,
-            swapThreshold: 0.65
-        });
-        entityMain.appendChild(entityItem);
+    let entityMap = [];
+    for(let i = 0; i<contentFields.length; i++){
+        contentFields[i].ref = fieldRender(contentFields[i].title);
+        entityMain.appendChild(contentFields[i].ref);
+        entityMap[contentFields[i].id] = contentFields[i];
+    }
+
+    for (id in entityMap) {
+        if( entityMap[id].parent != 0 ){
+            entityMap[entityMap[id].parent].ref.appendChild(entityMap[id].ref);
+        }
     }
 
 });
