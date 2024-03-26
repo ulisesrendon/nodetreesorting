dc35b29620aec95ab6575039d7463f27580a6345
Ulises Rendon <fdulises@outlook.com>
2024-03-26 17:02:57 -0600

    wip entity orderint workint

--- js/order.js
+++ js/order.js
@@ -2,9 +2,11 @@ const mainItem = document.createElement('div');
 mainItem.classList.add('list-group-item');
 mainItem.classList.add('nested');
 
-function fieldRender(title) {
+function fieldRender(field) {
     const newItem = mainItem.cloneNode();
-    newItem.innerHTML = `${title} <div class="list-group nested-sortable"></div>`;
+    newItem.setAttribute('data-id', field.id)
+    newItem.setAttribute('data-title', field.title)
+    newItem.innerHTML = `id_${field.id} ${field.title}`;
     new Sortable(newItem, {
         group: 'nested',
         animation: 150,
@@ -16,7 +18,16 @@ function fieldRender(title) {
 
 document.addEventListener('DOMContentLoaded', function(){
 
+    const entityOrderSave = document.querySelector("#entityOrderSave");
+
+    let fieldList = localStorage.getItem('fieldList') || contentFields;
+
+    if (typeof fieldList === 'string' ){
+        fieldList = JSON.parse(fieldList);
+    }
+
     const entityMain = document.querySelector("#entityMain");
+    entityMain.setAttribute('data-id', 0);
     new Sortable(entityMain, {
         group: 'nested',
         animation: 150,
@@ -25,16 +36,40 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 
     let entityMap = [];
-    for(let i = 0; i<contentFields.length; i++){
-        contentFields[i].ref = fieldRender(contentFields[i].title);
-        entityMain.appendChild(contentFields[i].ref);
-        entityMap[contentFields[i].id] = contentFields[i];
+    for(let i = 0; i<fieldList.length; i++){
+        entityMap[fieldList[i].id] = {
+            "render": fieldRender(fieldList[i]),
+            "data": fieldList[i]
+        };
+        entityMain.appendChild(entityMap[fieldList[i].id].render);
     }
 
     for (id in entityMap) {
-        if( entityMap[id].parent != 0 ){
-            entityMap[entityMap[id].parent].ref.appendChild(entityMap[id].ref);
+        if( entityMap[id].data.parent != 0 ){
+            entityMap[entityMap[id].data.parent].render.appendChild(entityMap[id].render);
         }
     }
+
+    entityOrderSave.addEventListener('click', function(){
+        for (id in entityMap) {
+            entityMap[id].weight = Array.from(entityMap[id].render.parentNode.children).indexOf(entityMap[id].render);
+        }
+
+        entityMap.sort(function(a,b){
+            return a.weight - b.weight;
+        });
+
+        let updatedFieldList = [];
+        for (id in entityMap) {
+            updatedFieldList.push({
+                "id": entityMap[id].data.id,
+                "title": entityMap[id].data.title,
+                "type": entityMap[id].data.type,
+                "parent": entityMap[id].render.parentNode.getAttribute('data-id')
+            });
+        }
+        localStorage.setItem('fieldList', JSON.stringify(updatedFieldList));
+    });
+
 
 });
